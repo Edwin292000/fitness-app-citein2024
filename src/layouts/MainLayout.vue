c714b2f3cd41e08e2e6425a5d1da5108e21c23f4
Edwin292000 <45053459+Edwin292000@users.noreply.github.com>
2024-11-14 23:30:17 -0600

    Proyecto para citein

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,51 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="bg-green-9">
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title> Fitness App </q-toolbar-title>
+
+        <div>Fitness App{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> Essential Links </q-item-label>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { useQuasar } from 'quasar';
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'MainLayout',
+  setup() {
+    const leftDrawerOpen = ref(false);
+    const $q = useQuasar();
+
+    return {
+      leftDrawerOpen,
+      $q,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,131 @@
+<template>
+  <q-page>
+    <q-pull-to-refresh @refresh="refresh" color="green">
+      <q-card>
+        <q-card-section>
+          <q-input v-model="tokenDevice" type="text" label="Token device" />
+        </q-card-section>
+        <q-card-section>
+          <q-select
+            v-model="exerciseType"
+            :options="['correr', 'nadar']"
+            label="Tipo de ejercicio"
+          />
+          <q-input
+            v-model="duration"
+            label="Duración (minutos)"
+            type="number"
+          />
+          <q-input v-model="distance" label="Distancia (km)" type="number" />
+          <br />
+          <div class="row">
+            <div class="col text-center">
+              <q-time v-model="at" color="green-9" />
+            </div>
+          </div>
+          <br />
+          <q-btn
+            @click="saveExercise"
+            label="Guardar ejercicio"
+            color="green-8"
+            class="fit"
+          />
+        </q-card-section>
+      </q-card>
+      <q-list>
+        <q-item v-for="exercise in exercises" :key="exercise.id">
+          <q-item-section>
+            <div>
+              {{ exercise.type }} - {{ exercise.duration }} min, A:{{
+                exercise.at
+              }}
+            </div>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-pull-to-refresh>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import { PushNotifications } from '@capacitor/push-notifications';
+interface exercise {
+  id: number;
+  type: string;
+  duration: number | null;
+  distance: number | null;
+  at: string | null;
+}
+export default defineComponent({
+  name: 'IndexPage',
+  components: {},
+  setup() {
+    return {
+      tokenDevice: ref(''),
+      exerciseType: ref(''),
+      duration: ref(null),
+      distance: ref(null),
+      exercises: ref<exercise[]>([]),
+      at: ref(null),
+    };
+  },
+  async mounted() {
+    await this.requestPushNotificationPermission();
+  },
+  methods: {
+    async requestPushNotificationPermission() {
+      const permission = await PushNotifications.requestPermissions();
+      if (permission.receive === 'granted') {
+        // Si el permiso es concedido, registramos el dispositivo
+        await PushNotifications.register();
+        this.addPushListeners();
+      } else {
+        console.log('Permiso de notificaciones no concedido.');
+      }
+    },
+    async addPushListeners() {
+      // Escuchar cuando se recibe una notificación push
+      PushNotifications.addListener(
+        'pushNotificationReceived',
+        (notification) => {
+          console.log('Notificación recibida:', notification);
+          alert(
+            `Notificación recibida: ${notification.title} - ${notification.body}`
+          );
+        }
+      );
+
+      // Escuchar cuando se recibe un token (ID único del dispositivo)
+      PushNotifications.addListener('registration', (token) => {
+        console.log('Token de notificación:', token.value);
+        this.tokenDevice = token.value;
+        //ffIOouJcRcKC4Ki48OnOEP:APA91bETWQCxEnwoznkdTw8amjBp9rETW2maMCSiGeZrKmglYAbz1Slbr3GSUzRytrNrZv46fhpXFEZ0eIubkKF0-n-EEpqr7xEQ-c27qpA5h7bo4bfFWWs
+        // Aquí puedes enviar este token a tu servidor para almacenar y luego enviar notificaciones
+      });
+    },
+    async refresh(done: () => void) {
+      await this.addPushListeners();
+
+      done();
+    },
+
+    sendNotification() {},
+
+    saveExercise() {
+      const newExercise: exercise = {
+        id: Date.now(),
+        type: this.exerciseType,
+        duration: this.duration,
+        distance: this.distance,
+        at: this.at,
+      };
+      this.exercises.push(newExercise);
+      this.exerciseType = '';
+      this.duration = null;
+      this.distance = null;
+      this.at = null;
+    },
+  },
+});
+</script>
